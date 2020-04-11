3965d536d9aace9f4e6da378b7cb5713e62d7ad7
sjgarza22 <sjgarza22@gmail.com>
2020-04-10 23:51:12 -0500

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,34 @@
 class Triangle
-  # write code here
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    if valid_triangle? != true
+      raise TriangleError
+    else
+      if @a == @b && @b == @c && @a == @c
+        :equilateral
+      elsif @a == @b || @b == @c || @a == @c
+        :isosceles
+      else
+        :scalene
+      end
+    end
+  end
+
+  def valid_triangle?
+    if @a > 0 && @b > 0 && @c > 0
+      if (@a + @b) > @c && (@b + @c) > @a && (@a + @c) > @b
+        true
+      end
+    else
+      false
+    end
+  end
+
+  class TriangleError < StandardError
+  end
 end
